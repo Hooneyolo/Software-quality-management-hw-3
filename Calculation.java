eae1447e1d039a7cabc85764d47ffaab9add58c5
YooJehun <dbwpgns0525@gmail.com>
2020-04-24 11:58:30 +0900

    Update Calculation

--- Calculation.java
+++ Calculation.java
@@ -14,7 +14,6 @@ public class Calculation
      * @return        최대값
      */
     public static int findMax(int[] array){  
-        
         int max = 0;  
         for(int i = 0; i < array.length; i++){  
             if(max < array[i])  
